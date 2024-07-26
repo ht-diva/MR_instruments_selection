2c9bb7f25b1afda3eec7139afecbd4f9b931685c
giulia.pontali <giulia.pontali@fht.org>
2024-07-26 10:18:57 +0200

    s04_liftover use the input and output file paths specified by Snakemake

--- SnakeMake/workflow/scripts/s04_liftover.py
+++ SnakeMake/workflow/scripts/s04_liftover.py
@@ -1,26 +1,56 @@
-import gwaslab as gl
+#import gwaslab as gl
 
 # Load the summary statistics - verbose if True print logs
-mysumstats = gl.Sumstats("results/MR_instruments_best_snps_from_LB.txt",
-                         snpid="SNPID",
-                         chrom="CHR",
-                         pos="POS",
-                         ea="EA",
-                         nea="NEA",
-                         neaf="EAF",
-                         beta="BETA",
-                         se="SE",
-                         mlog10p="MLOG10P",
-                         sep=",",
-                         verbose=True)
+#mysumstats = gl.Sumstats("results/MR_instruments_best_snps_from_LB.txt",
+#                         snpid="SNPID",
+#                         chrom="CHR",
+#                         pos="POS",
+#                        ea="EA",
+#                        nea="NEA",
+#                         neaf="EAF",
+#                         beta="BETA",
+#                         se="SE",
+#                         mlog10p="MLOG10P",
+#                         sep=",",
+#                         verbose=True)
 
-# See how to add how many SNPs we have and how many snps we loose after harmonization.
 # Perform basic checks
-mysumstats.basic_check(verbose=False)
+#mysumstats.basic_check(verbose=False)
 
 # Perform liftover, remove=True remove unmapped variants
-mysumstats.liftover(n_cores=3, from_build="19", to_build="38", remove=True)
+#mysumstats.liftover(n_cores=3, from_build="19", to_build="38", remove=True)
 
 # Save the output
 #mysumstats.write("MR_instruments_best_snps_from_LB_liftover.txt")
-mysumstats.to_format("results/MR_instruments_best_snps_from_LB_liftover.txt", fmt="gwaslab")
+#mysumstats.to_format("results/MR_instruments_best_snps_from_LB_liftover.txt", fmt="gwaslab")
+
+import gwaslab as gl
+
+# Snakemake automatically provides these variables
+input_file = snakemake.input[0]
+output_file = snakemake.output[0]
+
+params = snakemake.params
+
+# Load the summary statistics
+mysumstats = gl.Sumstats(input_file,
+                         snpid=params.snpid,
+                         chrom=params.chrom,
+                         pos=params.pos,
+                         ea=params.ea,
+                         nea=params.nea,
+                         neaf=params.neaf,
+                         beta=params.beta,
+                         se=params.se,
+                         mlog10p=params.mlog10p,
+                         sep=params.sep,
+                         verbose=True)
+
+# Perform basic checks
+mysumstats.basic_check(verbose=False)
+
+# Perform liftover
+mysumstats.liftover(n_cores=3, from_build=params.from_build, to_build=params.to_build, remove=True)
+
+# Save the output
+mysumstats.to_format(output_file, fmt="gwaslab")
