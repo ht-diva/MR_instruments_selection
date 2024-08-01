555a017b0fe9ede35f2d8603affd33a1ec43b357
giulia.pontali <giulia.pontali@fht.org>
2024-08-01 17:14:42 +0200

    changes to py file to try to understand why liftover is not able to find 4 snps

--- SnakeMake/workflow/scripts/s02_instruments_from_conditional_analysis.R
+++ SnakeMake/workflow/scripts/s02_instruments_from_conditional_analysis.R
@@ -5,14 +5,16 @@ source("workflow/scripts/s00_mapping_functions.R")
 
 option_list <- list(
   make_option("--input", default=NULL, help="Path and file name of Cojo"),
+  make_option("--mapping", default=NULL, help="Mapping file path for cis and trans")),
   make_option("--conditional_output", default=NULL, help="Output path and name for list of instruments from Cojo"),
   make_option("--unconditional_output", default=NULL, help="Output path and name for list of instruments from Cojo unconditional"),
-  make_option("--mapping", default=NULL, help="Mapping file path for cis and trans"))
+  make_option("--cojo_output", default=NULL, help="Output path from Cojo with cis and trans")
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
 cojo<-fread(opt$input)
 cond_path<-opt$conditional_output
 uncond_path<-opt$unconditional_output
+cojo_path<-opt$cojo_output
 mapping<-fread(opt$mapping)
 
 ##mapping file
@@ -28,19 +30,17 @@ for (i in 1:nrow(cojo)){
   cojo$cis_or_trans[i]<-map(cojo$study_id[i],cojo$Chr[i],cojo$bp[i],cojo$bp[i],mapping)
 }
 ##filtering only cis
-cojo<-cojo[which(cojo$cis_or_trans=="cis"),]
+cojo_cis<-cojo[which(cojo$cis_or_trans=="cis"),]
+##select unconditional beta se
+cojo_cis$Fstats_j<-((cojo_cis$bJ^2)/(cojo_cis$bJ_se^2))
+cojo_cis<-cojo_cis[which(cojo_cis$Fstats_j>10), ]
 
-##Filter p-value bonferroni corrected
-cojo_sign<-cojo[which(cojo$p < (5e-8/3958)), ]
-cojo_j_sign<- cojo[which(cojo$pJ < (5e-8/3958)), ]
-##Fstats computation and filtering
-cojo_sign$Fstats<-((cojo_sign$b^2)/(cojo_sign$se^2))
-cojo_sign<-cojo_sign[which(cojo_sign$Fstats>=10), ]
+cojo_conditional<-cojo_cis[, c("Chr","SNP","bp","refA","freq","n","freq_geno",
+                        "bJ","bJ_se","pJ","LD_r","snp_map","sdY","study_id","locus","EA", "Fstats_j")]
 
-cojo_j_sign$Fstats_j<-((cojo_j_sign$bJ^2)/(cojo_j_sign$bJ_se^2))
-cojo_j_sign<-cojo_j_sign[which(cojo_j_sign$Fstats_j>=10), ]
+cojo_unconditional<-cojo_cis[, c("Chr","SNP","bp","refA","freq","b","se","p","n","freq_geno",
+                        "LD_r","snp_map","sdY","study_id","locus","EA","Fstats_j")]
 
-write.table(cojo_sign,uncond_path)
-write.table(cojo_j_sign, cond_path)
-
-
+write.table(cojo_conditional, cond_path)
+write.table(cojo_un,uncond_path)
+write.table(cojo, cojo_path)
